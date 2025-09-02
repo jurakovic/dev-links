fde28db700169e5ec817005898a5358354e21d32
jurakovic <17744091+jurakovic@users.noreply.github.com>
2025-09-02 22:07:21 +0200

    function update

--- update_posts.sh
+++ update_posts.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-echo "test"
 
-yq -Version || {
-	echo "jq is not installed. Please install jq to proceed."
+function update() {
+  url=$1
+  name=$2
+  curl -s "$url" -H 'user-agent: Mozilla/5.0' | yq -p=xml -o=json '.rss.channel.item | select(length > 0) | .[:5] | map({"title": .title, "link": .link})' > $name.json
 }
 
-find posts
+yq -Version
+
+cd posts
+
+update "https://devblogs.microsoft.com/dotnet/feed/" "dotnet"
+update "https://devblogs.microsoft.com/visualstudio/feed/" "visualstudio"
+update "https://visualstudiomagazine.com/rss-feeds/news.aspx" "visualstudiomagazine"
+
+find .
