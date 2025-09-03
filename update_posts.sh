8a0ded1c02bc1e59a21b67b13cc25870006d7b20
jurakovic <17744091+jurakovic@users.noreply.github.com>
2025-09-03 15:39:14 +0200

    disable tidyfirst (cloudflare blocks curl from gh actions runners)

--- update_posts.sh
+++ update_posts.sh
@@ -5,14 +5,6 @@ function update() {
   name=$2
   echo "Processing feed: $name"
 
-
-  yq -Version
-  curl --version
-
-  curl -sI https://tidyfirst.substack.com/feed
-  curl -s --compressed -L -H 'user-agent: Mozilla/5.0' 'https://tidyfirst.substack.com/feed' | head -20
-  curl -s --compressed -L -H 'user-agent: Mozilla/5.0' 'https://tidyfirst.substack.com/feed'
-
   curl -s "$url" -H 'user-agent: Mozilla/5.0' | \
   yq -p=xml -o=json '
     (
@@ -47,39 +39,42 @@ function update() {
   fi
 }
 
+curl --version
+yq -Version
+
 cd posts
 
-#update "https://devblogs.microsoft.com/dotnet/feed/" "dotnet"
-#update "https://devblogs.microsoft.com/visualstudio/feed/" "visualstudio"
-#update "https://visualstudiomagazine.com/rss-feeds/news.aspx" "visualstudiomagazine"
-#update "https://microservices.io/feed.xml" "microservices"
-#update "https://www.developertoarchitect.com/lessons/index.xml" "developertoarchitect"
-#update "https://ardalis.com/rss.xml" "ardalis"
-#update "https://www.hanselman.com/blog/feed/rss" "hanselman"
-#update "https://andrewlock.net/rss.xml" "andrewlock"
-#update "https://davecallan.com/feed/" "davecallan"
-#update "https://feeds.feedburner.com/NitoPrograms" "stephencleary"
-#update "https://codeblog.jonskeet.uk/feed/" "jonskeet"
-#update "https://feeds.feedburner.com/grabbagoft" "jimmybogard"
-#update "https://www.devlead.se/feed.rss" "devlead"
-#update "https://steven-giesel.com/feed.rss" "steven-giesel"
-#update "https://adamsitnik.com/feed.xml" "adamsitnik"
-#update "https://minidump.net/index.xml" "minidump"
-#update "https://mattwarren.org/atom.xml" "mattwarren"
-#update "http://feeds.haacked.com/haacked" "haacked"
-#update "https://erikej.github.io/feed.xml" "erikej"
-#update "https://enterprisecraftsmanship.com/index.xml" "enterprisecraftsmanship"
-#update "https://antirez.com/rss" "antirez"
-#update "https://simonwillison.net/atom/everything/" "simonwillison"
-#update "https://nickcraver.com/blog/feed.xml" "nickcraver"
-#update "https://blog.codinghorror.com/rss/" "codinghorror"
-#update "https://www.joelonsoftware.com/feed/" "joelonsoftware"
-#update "https://neilonsoftware.com/feed/" "neilonsoftware"
-#update "https://www.brendangregg.com/blog/rss.xml" "brendangregg"
-update "https://tidyfirst.substack.com/feed" "tidyfirst"
-#update "https://feeds.feedburner.com/paulhammant" "paulhammant"
-#update "https://sizovs.net/feed.xml" "sizovs"
-#update "https://bartwullems.blogspot.com/feeds/posts/default" "bartwullems"
-#update "https://feeds.feedburner.com/ThePragmaticEngineer" "pragmaticengineer"
-#update "https://vadimkravcenko.com/feed/" "vadimkravcenko"
-#update "https://thecodist.com/rss/" "thecodist"
+update "https://devblogs.microsoft.com/dotnet/feed/" "dotnet"
+update "https://devblogs.microsoft.com/visualstudio/feed/" "visualstudio"
+update "https://visualstudiomagazine.com/rss-feeds/news.aspx" "visualstudiomagazine"
+update "https://microservices.io/feed.xml" "microservices"
+update "https://www.developertoarchitect.com/lessons/index.xml" "developertoarchitect"
+update "https://ardalis.com/rss.xml" "ardalis"
+update "https://www.hanselman.com/blog/feed/rss" "hanselman"
+update "https://andrewlock.net/rss.xml" "andrewlock"
+update "https://davecallan.com/feed/" "davecallan"
+update "https://feeds.feedburner.com/NitoPrograms" "stephencleary"
+update "https://codeblog.jonskeet.uk/feed/" "jonskeet"
+update "https://feeds.feedburner.com/grabbagoft" "jimmybogard"
+update "https://www.devlead.se/feed.rss" "devlead"
+update "https://steven-giesel.com/feed.rss" "steven-giesel"
+update "https://adamsitnik.com/feed.xml" "adamsitnik"
+update "https://minidump.net/index.xml" "minidump"
+update "https://mattwarren.org/atom.xml" "mattwarren"
+update "http://feeds.haacked.com/haacked" "haacked"
+update "https://erikej.github.io/feed.xml" "erikej"
+update "https://enterprisecraftsmanship.com/index.xml" "enterprisecraftsmanship"
+update "https://antirez.com/rss" "antirez"
+update "https://simonwillison.net/atom/everything/" "simonwillison"
+update "https://nickcraver.com/blog/feed.xml" "nickcraver"
+update "https://blog.codinghorror.com/rss/" "codinghorror"
+update "https://www.joelonsoftware.com/feed/" "joelonsoftware"
+update "https://neilonsoftware.com/feed/" "neilonsoftware"
+update "https://www.brendangregg.com/blog/rss.xml" "brendangregg"
+#update "https://tidyfirst.substack.com/feed" "tidyfirst" # cloudflare blocks curl
+update "https://feeds.feedburner.com/paulhammant" "paulhammant"
+update "https://sizovs.net/feed.xml" "sizovs"
+update "https://bartwullems.blogspot.com/feeds/posts/default" "bartwullems"
+update "https://feeds.feedburner.com/ThePragmaticEngineer" "pragmaticengineer"
+update "https://vadimkravcenko.com/feed/" "vadimkravcenko"
+update "https://thecodist.com/rss/" "thecodist"
